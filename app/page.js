c3d99057237282cfa4a34a40e9f06388b8cf72e3
kckonsta99 <kckonsta99@gmail.com>
2025-05-14 20:27:22 +0300

    Add wheel

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
 import Main from "@/components/Main";
+import Wheel from "@/components/Wheel";
 
 export default function HomePage() {
   return (
     <Main>
         <Hero />
+        <Wheel />
     </Main>
   );
 }
---
+++ components/Wheel.js
@@ -0,0 +1,70 @@
+'use client'
+import { useState, useRef } from 'react';
+
+const Wheel = () => {
+  const wheelRef = useRef(null); // Αναφορά για το στοιχείο τροχού
+  const [rotation, setRotation] = useState(0); // Αποθήκευση της περιστροφής
+
+  const spinWheel = () => {
+    const randomDeg = Math.floor(Math.random() * 360) + 1800; // Τουλάχιστον 5 γύροι
+    const newRotation = rotation + randomDeg;
+
+    setRotation(newRotation);
+
+    // Χρησιμοποιούμε την αναφορά του τροχού για να εφαρμόσουμε την περιστροφή
+    if (wheelRef.current) {
+      wheelRef.current.style.transition = "transform 4s cubic-bezier(0.33, 1, 0.68, 1)";
+      wheelRef.current.style.transform = `rotate(${newRotation}deg)`;
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col">
+        <p className='text-xl sm:text-2xl md:text-3xl text-center w-full mx-auto max-w-[800px] pb-8'>Let the wheel make the call!</p>
+        
+        <p className='text-4xl md:text-5xl font-bold'>🔻</p>
+      <div className="relative w-54 h-54 md:w-72 md:h-72">
+        <div
+          ref={wheelRef}
+          className="absolute inset-0 rounded-full bg-[#242323] transition-transform"
+          style={{ transform: `rotate(${rotation}deg)` }}
+        >
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom" style={{ transform: 'rotate(0deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3">🏀</span>
+          </div>
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom" style={{ transform: 'rotate(45deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3" >⚽️</span>
+          </div>
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom"  style={{ transform: 'rotate(90deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3">🏀</span>
+          </div>
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom"  style={{ transform: 'rotate(135deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3">⚽️</span>
+          </div>
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom"  style={{ transform: 'rotate(180deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3">🏀</span>
+          </div>
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom"  style={{ transform: 'rotate(225deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3">⚽️</span>
+          </div>
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom"  style={{ transform: 'rotate(270deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3">🏀</span>
+          </div>
+          <div className="absolute w-[1px] h-27 md:h-36 bg-white top-0 left-1/2 transform -translate-x-1/2 origin-bottom"  style={{ transform: 'rotate(315deg)' }}>
+            <span className="absolute text-4xl md:text-6xl py-3">⚽️</span>
+          </div>
+        </div>
+        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-[#ff4545] via-[#00ff99] to-[#006aff] animate-spin-slow opacity-50 blur-xl z-[-1]" />
+      </div>
+
+      <button
+        onClick={spinWheel}
+        className="mt-6 px-6 py-3 cursor-pointer flyingBtn"
+      >
+        Spin
+      </button>
+    </div>
+  );
+};
+
+export default Wheel;
